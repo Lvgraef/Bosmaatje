8ce825323af66db8803fe5e21529f960d2e19c7f
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-03-23 16:53:48 +0100

    progression

--- Assets/Scripts/TreatmentPlan/TreatmentPlanManager.cs
+++ Assets/Scripts/TreatmentPlan/TreatmentPlanManager.cs
@@ -1,8 +1,10 @@
-﻿using ApiClient;
+﻿using System.Collections.Generic;
+using ApiClient;
 using Dto;
 using TMPro;
 using Treatment;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace TreatmentPlan
 {
@@ -11,6 +13,7 @@ namespace TreatmentPlan
         public TextMeshProUGUI statusText;
         public TreatmentManager treatmentPrefab;
         public Transform canvas;
+        public List<Image> pos;
         public GetConfigurationsRequestDto Configuration { get; set; }
         public GetTreatmentRequestDto[] Treatments { get; set; }
 
@@ -18,6 +21,28 @@ namespace TreatmentPlan
         {
             Configuration = await ConfigurationApiClient.GetConfiguration();
             Treatments = await TreatmentPlanApiClient.GetTreatments(statusText, Configuration?.treatmentPlanName);
+            Progress();
+        }
+
+        private void Progress()
+        {
+            foreach (var image in pos)
+            {
+                image.gameObject.SetActive(false);
+            }
+            var sprite = Resources.Load<Sprite>(Configuration?.characterId);
+            int completion = 0;
+            for (var i = Treatments!.Length - 1; i >= 0; i--)
+            {
+                if (Treatments[i].isCompleted)
+                {
+                    completion = i;
+                    break;
+                }
+            }
+
+            pos[completion].sprite = sprite;
+            pos[completion].gameObject.SetActive(true);
         }
 
         public void OpenTreatment(int index)
