721f205c97759934ad4580137ed4c6ed334a37df
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-03-26 01:02:44 +0100

    Foundation of messages

---
+++ Assets/Scripts/Messages/Message.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+namespace Messages
+{
+    public class Message : MonoBehaviour
+    {
+        public TextMeshProUGUI typeText;
+        public TextMeshProUGUI messageText;
+        
+        public void OpenRandom()
+        {
+            var message = MessageData.Messages[Random.Range(0, MessageData.Messages.Count)];
+            typeText.text = message.Type;
+            messageText.text = message.Message;
+        }
+    }
+    
+    public class MessageData
+    {
+        public static readonly List<MessageData> Messages = new()
+        {
+            new MessageData("Tip", "Je mag je knuffel meenemen naar het ziekenhuis!"),
+            new MessageData("Advies", "Wanneer je veel zorgen ervaart, praat hier dan over met iemand die je vertrouwt."),
+            new MessageData("Herstel", "Wanneer je suiker laag is moet je een snoepje eten, dit kan je natuurlijk ook gezellig delen met klasgenootjes!"),
+        };
+        
+        public string Type { get; }
+        public string Message { get; }
+
+        private MessageData(string type, string message)
+        {
+            Type = type;
+            Message = message;
+        }
+    }
+}
