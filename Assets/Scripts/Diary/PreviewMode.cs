0210265b575c538aa4f155cd87fe4ff054f7dd42
lvgraef <86008039+itskillerluc@users.noreply.github.com>
2025-03-31 15:32:06 +0200

    trying to fix it

--- Assets/Scripts/Diary/PreviewMode.cs
+++ Assets/Scripts/Diary/PreviewMode.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-//using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using TMPro;
-using UnityEngine.UI;
+﻿using TMPro;
 using UnityEngine;
+//using System.Diagnostics;
 
 namespace Diary
 {
